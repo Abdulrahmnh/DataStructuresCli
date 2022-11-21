3a780607cf149d87e73a8048909b436ddef3c7be
Abdulrahman <AbdulrahmanAlhatti@gmail.com>
2022-11-21 15:47:37 +0300

    put exit choice and give some enhancement

--- DataStructuresCli/Program.cs
+++ DataStructuresCli/Program.cs
@@ -10,15 +10,34 @@ namespace DataStructuresCli
     {
         static void Main(string[] args)
         {
+            Dictionary<String, bool> dictionary = new Dictionary<string, bool>();
             Console.WriteLine("Type a name to give him/her what he/she needs");
             Console.WriteLine("Type exit at anytime to close the app");
 
             String input = Console.ReadLine();
-            Dictionary<String, bool> dictionary = new Dictionary<string, bool>();
-            dictionary.Add(input, true);
-            Console.WriteLine("Give " + input + " a laptop");
-            Console.WriteLine("Give " + input + " the project design");
 
+            while (input != "exit")
+            {
+                if (!dictionary.ContainsKey(input))
+                {
+                    dictionary.Add(input, true);
+                    Console.WriteLine("Give " + input + " a laptop");
+                    Console.WriteLine("Give " + input + " the project design");
+
+                    Console.WriteLine("Type a name to give him/her what he/she needs");
+                    Console.WriteLine("Type exit at anytime to close the app");
+                }
+                else
+                {
+                    Console.WriteLine(input + " is already added!");
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Here what you added!");
+            foreach (KeyValuePair<String, bool> item in dictionary)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
             Console.ReadKey();
         }
     }
