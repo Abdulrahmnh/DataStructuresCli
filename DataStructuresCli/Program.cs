b92643fc893a8b4e014425b423ebd430c87ec395
Abdulrahman <AbdulrahmanAlhatti@gmail.com>
2022-11-21 15:04:27 +0300

    first commit

---
+++ DataStructuresCli/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> names = new List<string>
+            {
+                "Mohammed",
+                "Khalid",
+                "Waleed",
+                "Nasser",
+                "Leen",
+                "Sara",
+                "Lujain",
+                "Shahad"};
+            foreach(String name in names)
+            {
+                Console.WriteLine("Give " + name + "a laptop");
+                Console.WriteLine("Give " + name + "the projct designs");
+            }
+            Console.ReadKey();
+        }
+    }
+}
